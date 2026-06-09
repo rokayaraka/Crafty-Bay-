9a3196b396d7ea9611dbe33ea17fef1ce18013a7
rokayaraka <rokayaraka251@gmail.com>
2026-06-09 20:55:10 +0600

    add navigator set up

---
+++ lib/app/crafty_bay_app.dart
@@ -0,0 +1,18 @@
+import 'package:crafty_bay/features/auth/presentation/screens/splash_screen.dart';
+
+import 'routes.dart';
+
+import 'package:flutter/material.dart';
+
+class CraftyBayApp extends StatelessWidget {
+  const CraftyBayApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Crafty Bay',
+      initialRoute: SplashScreen.name,
+      onGenerateRoute: AppRoutes.onGenerateRoute,
+    );
+  }
+}
